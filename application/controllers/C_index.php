a253846c6d03151b7c6f1ae6a268ac40d8f43201
dwifAl <alhusaenifitria14@gmail.com>
2023-11-19 19:09:25 +0700

    Update Simpulan data Anak di User Non Login

--- application/controllers/C_index.php
+++ application/controllers/C_index.php
@@ -3,10 +3,21 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class C_index extends CI_Controller
 {
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->library('form_validation');
+		$this->load->model("anak/M_anak","",TRUE);
+	}
+	
 	public function beranda()
 	{
 		$data = array(
-			'title' => 'Beranda | Sport Talent Prediction'
+			'title' => 'Beranda | Sport Talent Prediction',
+			'jmlDataAnak' => $this->M_anak->jumlahData(),
+			'jmlDataAnakLakiLaki' => $this->M_anak->jumlahDataLakiLaki(),
+			'jmlDataAnakPerempuan' => $this->M_anak->jumlahDataPerempuan(),
 		);
 		$this->load->view('v_index', $data);
 	}
