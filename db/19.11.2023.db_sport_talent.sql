1af7070e221ed8b5cc39e9ec6b7dbbe9e79aa9cf
dwifAl <alhusaenifitria14@gmail.com>
2023-11-19 18:08:07 +0700

    update db

--- db/19.11.2023.db_sport_talent.sql
+++ db/19.11.2023.db_sport_talent.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 19, 2023 at 07:46 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Nov 19, 2023 at 12:07 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,73 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_anak`
+--
+
+CREATE TABLE `tb_anak` (
+  `id_anak` int(11) NOT NULL,
+  `nama_anak` varchar(255) NOT NULL,
+  `jenis_kelamin_anak` char(1) NOT NULL,
+  `tinggi_badan_anak` int(11) NOT NULL,
+  `berat_badan_anak` int(11) NOT NULL,
+  `tinggi_duduk_anak` int(11) NOT NULL,
+  `rentang_lengan_anak` int(11) NOT NULL,
+  `kecepatan_anak` int(11) NOT NULL,
+  `kelincahan_anak` int(11) NOT NULL,
+  `koordinasi_anak` int(11) NOT NULL,
+  `kekuatan_anak` int(11) NOT NULL,
+  `power_anak` int(11) NOT NULL,
+  `daya_tahan_anak` int(11) NOT NULL,
+  `nilai_potensi_anak` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_anak`
+--
+
+INSERT INTO `tb_anak` (`id_anak`, `nama_anak`, `jenis_kelamin_anak`, `tinggi_badan_anak`, `berat_badan_anak`, `tinggi_duduk_anak`, `rentang_lengan_anak`, `kecepatan_anak`, `kelincahan_anak`, `koordinasi_anak`, `kekuatan_anak`, `power_anak`, `daya_tahan_anak`, `nilai_potensi_anak`) VALUES
+(5, 'Putri', '2', 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9),
+(6, 'yendra', '1', 9, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9),
+(7, 'Dwi', '1', 8, 9, 8, 9, 9, 9, 9, 9, 9, 8, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_cabor`
+--
+
+CREATE TABLE `tb_cabor` (
+  `id_cabor` int(11) NOT NULL,
+  `nama_cabor` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_cabor`
+--
+
+INSERT INTO `tb_cabor` (`id_cabor`, `nama_cabor`) VALUES
+(1, 'Bola Basket'),
+(2, 'Bola Voli'),
+(3, 'Bulutangkis'),
+(4, 'Sepakbola'),
+(5, 'Beladiri');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_tokens`
+--
+
+CREATE TABLE `tb_tokens` (
+  `id_token` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `email_user` varchar(50) NOT NULL,
+  `created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_user`
 --
 
@@ -31,52 +98,62 @@ CREATE TABLE `tb_user` (
   `id_user` int(11) NOT NULL,
   `name_user` varchar(128) NOT NULL,
   `email_user` varchar(128) NOT NULL,
+  `image_user` varchar(128) NOT NULL,
   `password_user` varchar(256) NOT NULL,
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
-  `date_created` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`id_user`, `name_user`, `email_user`, `password_user`, `role_id`, `is_active`, `date_created`) VALUES
-(1, 'Yendra Priambada', 'yendra@gmail.com', '$2y$10$o0/aWvDmNeE7mD0GGnHMX.RwJx9QoIs0v0HrD4ODRQJWru34w0Tty', 2, 1, '0000-00-00 00:00:00'),
-(5, 'Dwi Fitria Al Husaeni', 'dwi@gmail.com', '$2y$10$quXydg/Tlfajxit6lOVRwO6.A4Ph7UFxe4ie/XP/RjAJuSP7itrh.', 1, 1, '0000-00-00 00:00:00'),
-(7, 'Admin', 'admin@gmail.com', '$2y$10$xKu.d9TwmjNuoefNU39YWekt7IZZNclDS5Otw9IpR9QHgteQBIfW6', 1, 1, '0000-00-00 00:00:00'),
-(15, 'Akun Member', 'member@gmail.com', '$2y$10$QTQ8aJbxOSnBF3nX/74sa.quwojQyS7CwnEvW4NaeQs//5Uv8xuvO', 2, 1, '0000-00-00 00:00:00'),
-(16, 'Akun Quick Access', 'quick@gmail.com', '$2y$10$E8fpXH/YYY9U7aSD0FbU6ea6GylWG1.mm3Dtj4vdGiKIbkzKGKAKu', 3, 1, '0000-00-00 00:00:00'),
-(17, 'test', 'test@gmail.com', '$2y$10$uZ310SsuqrfEXlf45CR30eti7fB9G5JfFaFvIN/A8UBMJU1lHAenm', 1, 1, '0000-00-00 00:00:00'),
-(18, 'ASDGAS', '1@uhsdf.ds', '$2y$10$t0DxwJrZhjFUVLD6zZS97Odm67pgjcXBYcYzUU8ksR10OLeNx9b8u', 2, 0, '0000-00-00 00:00:00'),
-(19, 'adsf', 'uyhij@Uhj.dsf', '$2y$10$12SAbsSA2cqHsQYeHg4La.8/YOM6d7hmAWTmyXaxWTRnpfg2Zcgma', 2, 0, '0000-00-00 00:00:00'),
-(20, 'q', 'rw@uhn.dsf', '$2y$10$4TdOVDoDuePgGC7J14RzSOe.jQ3IT8WvQOXbVa7fU1378yE2U23VO', 2, 0, '0000-00-00 00:00:00'),
-(21, 'guh', 'uuhg@ughj.dsf', '$2y$10$NFIK5HnaCdjgEOZCZIzzMOVy/A3VMFFFIOg.ZISD19sgqA/fgWubW', 2, 0, '0000-00-00 00:00:00'),
-(22, 'fhgh', 'ghb@yhujk.sd', '$2y$10$TLwup6iZnSlr7eMId3DnYula1rB9l8bW4y41DSjcZ1lo7YJeo6fiu', 3, 0, '2023-11-19 01:47:51');
+INSERT INTO `tb_user` (`id_user`, `name_user`, `email_user`, `image_user`, `password_user`, `role_id`, `is_active`, `date_created`) VALUES
+(1, 'Yendra', 'yendra@gmail.com', 'default.png', '$2y$10$o0/aWvDmNeE7mD0GGnHMX.RwJx9QoIs0v0HrD4ODRQJWru34w0Tty', 2, 1, 1699328307),
+(5, 'Dwi', 'dwi@gmail.com', 'default.png', '$2y$10$quXydg/Tlfajxit6lOVRwO6.A4Ph7UFxe4ie/XP/RjAJuSP7itrh.', 1, 1, 1699335784);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_user_role`
+-- Table structure for table `user_role`
 --
 
-CREATE TABLE `tb_user_role` (
+CREATE TABLE `user_role` (
   `id` int(11) NOT NULL,
   `role` varchar(128) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tb_user_role`
+-- Dumping data for table `user_role`
 --
 
-INSERT INTO `tb_user_role` (`id`, `role`) VALUES
+INSERT INTO `user_role` (`id`, `role`) VALUES
 (1, 'Administrator'),
 (2, 'Member'),
-(3, 'Unverfied User');
+(3, 'Guest');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tb_anak`
+--
+ALTER TABLE `tb_anak`
+  ADD PRIMARY KEY (`id_anak`);
+
+--
+-- Indexes for table `tb_cabor`
+--
+ALTER TABLE `tb_cabor`
+  ADD PRIMARY KEY (`id_cabor`);
+
+--
+-- Indexes for table `tb_tokens`
+--
+ALTER TABLE `tb_tokens`
+  ADD PRIMARY KEY (`id_token`);
 
 --
 -- Indexes for table `tb_user`
@@ -85,9 +162,9 @@ ALTER TABLE `tb_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- Indexes for table `tb_user_role`
+-- Indexes for table `user_role`
 --
-ALTER TABLE `tb_user_role`
+ALTER TABLE `user_role`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -95,15 +172,33 @@ ALTER TABLE `tb_user_role`
 --
 
 --
+-- AUTO_INCREMENT for table `tb_anak`
+--
+ALTER TABLE `tb_anak`
+  MODIFY `id_anak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_cabor`
+--
+ALTER TABLE `tb_cabor`
+  MODIFY `id_cabor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_tokens`
+--
+ALTER TABLE `tb_tokens`
+  MODIFY `id_token` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `tb_user_role`
+-- AUTO_INCREMENT for table `user_role`
 --
-ALTER TABLE `tb_user_role`
+ALTER TABLE `user_role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
